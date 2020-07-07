b6fc701d79f0d611578b3b7fb60da291f2247d6f
Sacha Sedriks <sachased@gmail.com>
2020-07-07 13:54:56 -0700

    installed winston to perform logs; added mock test data; added GET endpoints for /card/:id and /list/:id.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
+const winston = require('winston')
 const { NODE_ENV } = require('./config')
 
 const app = express()
@@ -14,10 +15,112 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+// set up winston
+const logger = winston.createLogger({
+  level: 'info',  // winston has six levels of severity: silly, debug, verbose, info, warn, and error.
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: 'info.log' })
+  ]
+})
+
+if (NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple()
+  }));
+}
+
+// VALIDATION MIDDLEWARE
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`); // new via winston
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+
+  next()
+})
+
+
+// TEST DATA /////////////////////////////////
+const cards = [
+  {
+    id: 1,
+    title: 'Task One',
+    content: 'This is card one',
+  },
+  {
+    id: 2,
+    title: 'Task Two',
+    content: 'This is card two',
+  },
+  {
+    id: 3,
+    title: 'Task Three',
+    content: 'This is card three',
+  },
+];
+const lists = [
+  {
+    id: 1,
+    header: 'List One',
+    cardIds: [1],
+  },
+  {
+    id: 2,
+    header: 'List Two',
+    cardIds: [2, 3],
+  },
+]
+// ^^ TEST DATA /////////////////////////////////
+
 
 app.get('/', (req, res) => {
   res.send('Hello, world!')
 })
+
+app.get('/card', (req, res) => {
+  res
+    .json(cards)
+})
+
+app.get('/list', (req, res) => {
+  res
+    .json(lists)
+})
+
+app.get('/card/:id', (req, res) => {
+  const { id } = req.params
+  const card = cards.find(c => c.id == id); // NOTE use of equality operator (==) instead of strict equality operator (===), for auto type coercion.
+
+  if (!card) {
+    logger.error(`Card with id ${id} not found.`)
+    return res
+      .status(404)
+      .send('Card Not Found')
+  }
+  res
+    .status(200)
+    .json(card)
+})
+
+app.get('/list/:id', (req, res) => {
+  const { id } = req.params
+  const list = lists.find(li => li.id == id)
+  
+  if(!list) {
+    logger.error(`List with id ${id} not found`)
+    return res
+      .status(400)
+      .send('List Not Found')
+  }
+  res
+    .status(200)
+    .json(list)
+})
+
 
 app.use(function errorHandler(error, req, res, next) {
   let response
